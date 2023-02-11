147bc849582b8d5cc1270e5db946b6c18bbbc288
Alexandr <ftr15@yandex.ru>
2023-02-12 00:22:01 +0700

    done hometask4

---
+++ Seminar4/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+double n = Convert.ToInt32(Console.ReadLine()), result = 1;
+for (int i = 1; i <= n; i++)
+  result = result * i; // result *= i;
+Console.WriteLine($"Результат: {result}");
---
+++ Seminar4/task25/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Нельзя использовать Math.Pow();
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+
+Console.Clear();
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine()), i = 0;
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+while (i < B)
+{
+    result = result * A;
+    i++;
+}
+Console.WriteLine($"Число A в степени B равно: {result}");
