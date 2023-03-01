decb2dfda7de44f98b3620da65a7aad55ec1560d
Alexandr <ftr15@yandex.ru>
2023-03-01 18:27:04 +0700

    done hometask6

---
+++ Seminar6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine()), a = 0, b = 1, x;
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"{a} ");
+    x = a + b;
+    a = b;
+    b = x;
+    
+}
---
+++ Seminar6/task41/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.Clear();
+Console.Write("Введите количество чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[m];
+Console.WriteLine("Введите числа: ");
+for (int i = 0; i < m; i++)
+{
+array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int count = 0;
+for (int i = 0; i < m; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+
+}
+
+Console.Write($"Количество чисел больше 0 равно: {count}");
