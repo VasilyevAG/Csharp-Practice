a29b9125a4f51e2f9e2a9161f80d1cabd11617d6
Alexandr <ftr15@yandex.ru>
2023-03-03 13:54:59 +0700

    done hometask9

---
+++ Seminar9/Program.cs
@@ -0,0 +1,14 @@
+﻿int Stepen(int a, int b)
+{
+    if (b == 0)
+        return 1;
+    return Stepen(a, b - 1) * a;
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Stepen(a, b));
---
+++ Seminar9/task66/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int Summ(int m, int n)
+{
+    if (n == m)
+        return (m);
+    return m + Summ((m + 1), n);
+}
+
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма натуральных элементов в промежутке от {m} до {n} равна {Summ(m, n)}");
